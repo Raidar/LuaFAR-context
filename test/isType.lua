83f69474c5ac871f2a81072ed0799f775fdb4a80
Aidar Rakhmatullin <raidar@yandex.ru>
2012-05-19 23:20:15 +0400

    Check isType script

--- test/isType.lua
+++ test/isType.lua
@@ -9,7 +9,15 @@
 local meta = getmetatable('').__index or string
 meta.isType = context.detect.use.isType
 
-local tps = { 'lua', 'main', 'config', 'ini', 'reg', 'awk' }
+local tps = {
+  'lua',
+  'main',
+  'config',
+  'ini',
+  'reg',
+  'awk',
+  --'script',
+} ---
 local t = {}
 
 for _, t1 in pairs(tps) do
@@ -17,6 +25,7 @@ for _, t1 in pairs(tps) do
      t[#t+1] = ('%s %s %s'):format(t1, t1:isType(t2) and 'is' or 'is not', t2)
   end
 end
+t[#t+1] = "--- end ---"
 
 far.Message(table.concat(t, '\n'), "isType test")
 --------------------------------------------------------------------------------
