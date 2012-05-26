9d75aff65be6fa63a9143b8b5f67120ec258348e
Aidar Rakhmatullin <raidar@yandex.ru>
2012-05-26 08:17:25 +0400

    Show far2.keynames for virkeys

---
+++ samples/virkeys.lua
@@ -0,0 +1,47 @@
+-- Key names show
+
+local F = far.Flags
+local format = string.format
+
+local fkeys = require "far2.keynames"
+
+local VKeys = win.GetVirtualKeys()
+
+local count = 0
+for k, v in pairs(VKeys) do
+  if type(k) == 'number' and k > count then
+    count = k
+  end
+end
+
+local line = "----------------------------------------"
+local t = { line }
+
+for k = 1, count do
+  local v = VKeys[k]
+  if v then
+    local Input = {
+      EventType       = F.KEY_EVENT,
+      --KeyDown         = true,
+      --RepeatCount     = 1,
+      VirtualKeyCode  = k,
+      --VirtualScanCode = 0x00,
+      --UnicodeChar     = "",
+      ControlKeyState = 0x00,
+    } ---
+    local s = fkeys.InputToKeyName(Input) or ""
+    if s:len() <= 1 or s ~= s:upper() then
+      t[#t + 1] = format("%s -> %s", v, s)
+      Input.ControlKeyState = 0x0100 -- ENHANCED_KEY
+      s = fkeys.InputToKeyName(Input) or ""
+      local s = format("%s -> %s", v, s)
+      if s ~= t[#t] then
+        t[#t + 1] = s.." (enhanced)"
+      end
+    end
+  end
+end
+
+t[#t + 1] = line
+
+far.Show(unpack(t))
