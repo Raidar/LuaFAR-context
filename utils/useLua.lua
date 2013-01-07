bef1e2de4607291a2a71ee83969b5ea0ba58b373
Aidar Rakhmatullin <raidar@yandex.ru>
2013-01-07 21:59:52 +0400

    Add some information about lua regex

--- utils/useLua.lua
+++ utils/useLua.lua
@@ -24,9 +24,11 @@ local logShow = log.Show
 --------------------------------------------------------------------------------
 local unit = {}
 
----------------------------------------- const
+----------------------------------------
+local concat = table.concat
 local format = string.format
 
+---------------------------------------- const
 local IdentFirstChars = "a-zA-Z_"
 local IdentOtherChars = "a-zA-Z_0-9"
 
@@ -74,10 +76,35 @@ unit.types = {
   ["thread"] = true,
 } -- types
 
+unit.regex = {
+  -- Lua character %-classes:
+  ClassList = { '%c', '%a', '%d', '%s', '%p',
+                '%l', '%u', '%w', '%x', '%z' },
+  -- Lua wildcard characters:
+  CardsList = { '?', '*', '+', '-' },
+  Cards     = "", -- a string of them
+  CardsSet  = "", -- a set of them
+
+  DefCharEnum = "%w_",   -- Acceptable word characters
+  -- Patterns:
+  CharSetPat  = "[%s]",  -- for set of included characters
+  NoneSetPat  = "[^%s]", -- for set of excluded characters
+} -- regex
+local regex = unit.regex
+
+regex.Cards    = concat(regex.CardsList)
+regex.CardsSet = format(regex.CharSetPat, regex.Cards:gsub("(.)", "%%%1"))
+
+--logShow(regex.CardsSet, regex.Cards)
+
 ---------------------------------------- utils
 local TextFirstCharMask = format("^[^%s]", IdentFirstChars)
 local TextOtherCharMask = format("[^%s]", IdentOtherChars)
 
+-- Convert a string to identifier
+-- replacing unacceptable chars with '_'.
+-- Преобразование строки в идентификатор
+-- путём замены неприемлемых символов на '_'.
 function unit.NameToIdent (name) --> (string)
   return name:gsub(TextOtherCharMask, "_")
              :gsub(TextFirstCharMask, "_%1")
