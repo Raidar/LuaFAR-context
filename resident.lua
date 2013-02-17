9257a52cbecbbb15afcd5eb1a0940272beef7b4d
Aidar Rakhmatullin <raidar@yandex.ru>
2013-02-17 11:59:03 +0400

    Comment require log

--- resident.lua
+++ resident.lua
@@ -19,7 +19,7 @@ MakeResident(require "context.resident") -- LFc resident
 --
 --]]
 ----------------------------------------
--- [[
+--[[
 local log = require "context.samples.logging"
 local logShow = log.Show
 --]]
